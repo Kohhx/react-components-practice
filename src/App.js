a54786e311b3e227a27393980129df7c2bccb926
He Xiang Koh <kohhxx6@gmail.com>
2023-01-19 20:41:34 +0800

    added proptypes package for props validation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <div>
+        <Button primary>Clicked Me!</Button>
+      </div>
+      <div>
+        <Button>Buy Me!</Button>
+      </div>
+      <div>
+        <Button>Hey!</Button>
+      </div>
+      <div>
+        <Button>There</Button>
+      </div>
+      <div>
+        <Button>Delete</Button>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
