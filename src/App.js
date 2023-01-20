076fca891b6ef8ed8a0f37eb1a64806ddf390c68
He Xiang Koh <kohhxx6@gmail.com>
2023-01-20 11:49:19 +0800

    Completed accordion component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
-import ButtonPage from "./pages/ButtonPage"
+import ButtonPage from "./pages/ButtonPage";
+import Accordion from "./components/Accordion";
 import { FaAccessibleIcon } from "react-icons/fa";
 
 const App = () => {
-
-
+  const items = [
+    {
+      id: 1,
+      label: "Can i use react?",
+      content: "you can!",
+    },
+    {
+      id: 2,
+      label: "Can i use Node?",
+      content: "Nope!",
+    },
+    {
+      id: 3,
+      label: "Can i use Angular?",
+      content: "SO difficult!",
+    },
+  ];
 
   return (
-   <div><ButtonPage /></div>
+    //  <div><ButtonPage /></div>
+    <Accordion items={items} />
   );
 };
 
---
+++ src/components/Accordion.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { GoChevronLeft } from "react-icons/go";
+import { GoChevronDown } from "react-icons/go";
+
+const Accordion = ({ items }) => {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  // Create function outside mapping function. Use this method
+  const handleClick = (index) => {
+    if (expandedIndex === index) {
+      setExpandedIndex(-1);
+    } else {
+      setExpandedIndex(index);
+    }
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    const icon = (
+      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    );
+
+    return (
+      // <div key={item.id} onClick={() => setExpandedIndex(index)}>
+      <div key={item.id}>
+        <div
+          onClick={() => handleClick(index)}
+          className="flex items-center p-3 bg-gray-50 border-b cursor-pointer justify-between"
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
+      </div>
+    );
+  });
+
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
+};
+
+export default Accordion;
